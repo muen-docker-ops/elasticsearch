54acd92a39722e8a50a68bb3cabf57b0cdef990d
buyfakett <buyfakett@vip.qq.com>
2024-11-02 13:20:03 +0800

    更新

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -f elasticsearch-1.7.0.zip ]; then
-    wget 
+    wget https://github.com/muen-docker-ops/elasticsearch/releases/download/1.0.0/elasticsearch-1.7.0.zip
     unzip elasticsearch-1.7.0.zip
 fi
